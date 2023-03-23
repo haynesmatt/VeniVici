7713d3a971bf26985cea121dab232880645536c3
Matt <matthaynes412@gmail.com>
2023-03-23 00:59:12 -0400

    setup api request w/ axios from cat api

--- src/components/BanList.jsx
+++ src/components/BanList.jsx
@@ -7,7 +7,10 @@ const BanList = () => {
         <div className='BanList'>
 
             <h2>Ban List</h2>
-            <h3>Select an attribute in your listing to ban it</h3>
+            <h3>Select an attribute in your listing to ban it.</h3>
+            <div className='attributes'>
+                <button className='attribute'>Somali</button>
+            </div>
 
         </div>
 
--- src/components/Discovery.jsx
+++ src/components/Discovery.jsx
@@ -1,15 +1,37 @@
 import './Discovery.css'
+import axios from 'axios'
+import { useState, useEffect } from 'react';
 
 const Discovery = () => {
+
+    const URL = 'https://api.thecatapi.com/v1/images/search';
+
+    axios.get(URL).then((response)=> console.log(response) ).catch(error => console.error(error));
+
+    const [image, setImage] = useState('https://cdn2.thecatapi.com/images/4hb.gif');
+
+    const handleDiscoverClick = () => {
+        axios.get(URL)
+            .then(response => {
+                setImage(response.data[0].url);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    };
 
     return (
 
         <div className='Discovery'>
 
           <h1>Veni Vici!</h1>
-          <h2>Discover cats from your wildest dreams!</h2>
+          <h2>Cat</h2>
+          <div className='attributes'>
+            <button className='attribute'>Somali</button>
+          </div>
+          <img src={image}></img>
 
-          <button>Discover!</button>
+          <button onClick={handleDiscoverClick} className='discover'>Discover!</button>
 
         </div>
 
